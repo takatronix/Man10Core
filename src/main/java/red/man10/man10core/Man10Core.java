14c824a1913eaf178856f02f5fb2757255a47967
Takashi Ohtsuka <takatronix@gmail.com>
2017-02-28 05:33:48 +0900

    ジョインイベント処理追加

--- src/main/java/red/man10/man10core/Man10Core.java
+++ src/main/java/red/man10/man10core/Man10Core.java
@@ -1,8 +1,12 @@
 package red.man10.man10core;
 
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Man10Core extends JavaPlugin {
@@ -30,7 +34,9 @@ public final class Man10Core extends JavaPlugin {
         return;
     }
 
+    /////////////////////////////////
     //      起動
+    /////////////////////////////////
     @Override
     public void onEnable() {
         getLogger().info("Enabled");
@@ -38,12 +44,17 @@ public final class Man10Core extends JavaPlugin {
         loadConfig();
     }
 
+    /////////////////////////////////
     //      終了
+    /////////////////////////////////
     @Override
     public void onDisable() {
         getLogger().info("Disabled");
     }
+
+    /////////////////////////////////
     //      コマンド処理
+    /////////////////////////////////
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
         Player p = (Player) sender;
@@ -51,5 +62,21 @@ public final class Man10Core extends JavaPlugin {
         return true;
     }
 
-
+    /////////////////////////////////
+    //     ジョインイベント
+    /////////////////////////////////
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent e){
+        Player p = e.getPlayer();
+        p.sendMessage(ChatColor.RED + "Welcome to Man10 Server");
+    }
+    /////////////////////////////////
+    //      チャットイベント
+    /////////////////////////////////
+    @EventHandler
+    public void onChat(AsyncPlayerChatEvent e) {
+        Player p = e.getPlayer();
+        String message = e.getMessage();
+        p.sendMessage(ChatColor.RED + message);
+    }
 }
