b4cd0ee35c77df3147e6f0de0284abf19259a4e0
Takashi Ohtsuka <takatronix@gmail.com>
2017-02-28 05:18:43 +0900

    mysql 設定読み込み処理

--- src/main/java/red/man10/man10core/Man10Core.java
+++ src/main/java/red/man10/man10core/Man10Core.java
@@ -1,17 +1,55 @@
 package red.man10.man10core;
 
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Man10Core extends JavaPlugin {
 
-        @Override
-        public void onEnable() {
-            // Plugin startup logic
+    /////////////////////////////////
+    //        設定値
+    /////////////////////////////////
+    String  mysql_ip;
+    String  mysql_port;
+    String  mysql_id;
+    String  mysql_pass;
+    String  mysql_dbname;
 
-        }
+    /////////////////////////////////
+    //       設定ファイル読み込み
+    /////////////////////////////////
+    public void loadConfig(){
+        this.reloadConfig();
+        mysql_ip = this.getConfig().getString("server_config.mysql_ip");
+        mysql_port = this.getConfig().getString("server_config.mysql_port");
+        mysql_id = this.getConfig().getString("server_config.mysql_id");
+        mysql_pass = this.getConfig().getString("server_config.mysql_pass");
+        mysql_dbname = this.getConfig().getString("server_config.db_name");
+        getLogger().info("Config loaded");
+        return;
+    }
 
-        @Override
-        public void onDisable() {
-            // Plugin shutdown logic
-        }
+    //      起動
+    @Override
+    public void onEnable() {
+        getLogger().info("Enabled");
+        this.saveDefaultConfig();
+        loadConfig();
+    }
+
+    //      終了
+    @Override
+    public void onDisable() {
+        getLogger().info("Disabled");
+    }
+    //      コマンド処理
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        Player p = (Player) sender;
+
+        return true;
+    }
+
+
 }
