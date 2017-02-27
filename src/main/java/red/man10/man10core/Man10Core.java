1c46944d38ae4bca3b709a857edf6e7a77989589
Takashi Ohtsuka <takatronix@gmail.com>
2017-02-27 14:28:39 +0900

    Initial commit

---
+++ src/main/java/red/man10/man10core/Man10Core.java
@@ -0,0 +1,17 @@
+package red.man10.man10core;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Man10Core extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
