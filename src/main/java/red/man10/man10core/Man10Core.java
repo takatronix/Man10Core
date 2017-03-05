4b69ffd7e24f453ae0de10503ae933fa028755ba
Takashi Ohtsuka <takatronix@gmail.com>
2017-03-05 10:58:25 +0900

    ログイン履歴をmysqlに保存する

--- src/main/java/red/man10/man10core/Man10Core.java
+++ src/main/java/red/man10/man10core/Man10Core.java
@@ -1,5 +1,6 @@
 package red.man10.man10core;
 
+import eu.theindra.geoip.GeoIP;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 
@@ -13,6 +14,7 @@ import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.inventivetalent.glow.GlowAPI;
 
+import java.net.InetAddress;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
@@ -35,7 +37,7 @@ public final class Man10Core extends JavaPlugin implements Listener {
 
         vault = new VaultManager(this);
         mysql = new MySQLManager(this,"Man10Core");
-        mysql.debugMode = true;
+        //mysql.debugMode = true;
         //   テーブル作成
         createTables();
 
@@ -65,7 +67,25 @@ public final class Man10Core extends JavaPlugin implements Listener {
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent e){
         Player p = e.getPlayer();
-        p.sendMessage(ChatColor.YELLOW  + "Welcome to Man10 Server");
+        p.sendMessage(ChatColor.YELLOW  + "OHAMAN!! Welcome to Man10 Server / おはまん！！ まんじゅうサーバへようこそ！");
+
+
+        ////////////////////////////////
+        //      GeoIPチェック
+        ////////////////////////////////
+        InetAddress ip = p.getAddress().getAddress();
+        String userip = p.getAddress().getAddress().toString().substring(1);
+        String country = GeoIP.getCountry(ip);
+        String city = GeoIP.getCity(ip);
+        String timezone = GeoIP.getTimezone(ip);
+        double balance = vault.getBalance(p.getUniqueId());
+        //      ログインログ保存
+        insertLoginLog(Bukkit.getServerName(),p.getWorld().getName(),p.getName(),userip,country,city,timezone,balance,p.getUniqueId().toString());
+
+        GlowAPI.setGlowing(e.getPlayer(), GlowAPI.Color.RED, Bukkit.getOnlinePlayers());
+
+
+
     }
     /////////////////////////////////
     //      チャットイベント
@@ -74,43 +94,93 @@ public final class Man10Core extends JavaPlugin implements Listener {
     public void onChat(AsyncPlayerChatEvent e) {
         Player p = e.getPlayer();
         String message = e.getMessage();
-        if(message.contains("ohaman")){
+        if(message.contains("ohaman") || message.contains("おはまん")){
             p.sendMessage("あいさつしたので国王からお小遣いをもらった");
             vault.deposit(e.getPlayer().getUniqueId(),10000);
           //  vaultManager.showBalance(e.getPlayer().getUniqueId());
             return;
         }
 
-        GlowAPI.setGlowing(e.getPlayer(), GlowAPI.Color.RED, Bukkit.getOnlinePlayers());
+
+
        // createTables();
-        insertMessage(Bukkit.getServerName(), p.getWorld().getName(),p.getName(),message);
+        insertChatLog(Bukkit.getServerName(), p.getWorld().getName(),p.getName(),message);
     }
 
-    String sqlMessageTable = "CREATE TABLE `chat_log` (\n" +
+
+    /////////////////////////////////////////
+    //      チャットログテーブル
+    /////////////////////////////////////////
+    String sqlChatTable = "CREATE TABLE `chat_log` (\n" +
             "  `id` int(11) NOT NULL AUTO_INCREMENT,\n" +
             "  `server` varchar(100) DEFAULT NULL,\n" +
             "  `world` varchar(100) DEFAULT NULL,\n" +
             "  `name` varchar(100) DEFAULT NULL,\n" +
             "  `message` varchar(400) DEFAULT NULL,\n" +
-            "  `timestamp` varchar(50) DEFAULT NULL,\n" +
+            "  `timestamp` int(50) DEFAULT NULL,\n" +
+            "  PRIMARY KEY (`id`)\n" +
+            ") ENGINE=InnoDB AUTO_INCREMENT=188 DEFAULT CHARSET=utf8;";
+    ///////////////////////////////////////////
+    //      ログインテーブル
+    ///////////////////////////////////////////
+    String sqlLoginTable = "CREATE TABLE `login_log` (\n" +
+            "  `id` int(11) NOT NULL AUTO_INCREMENT,\n" +
+            "  `server` varchar(100) DEFAULT NULL,\n" +
+            "  `world` varchar(100) DEFAULT NULL,\n" +
+            "  `name` varchar(100) DEFAULT NULL,\n" +
+            "  `ip` varchar(100) DEFAULT NULL,\n" +
+            "  `country` varchar(100) DEFAULT NULL,\n" +
+            "  `city` varchar(100) DEFAULT NULL,\n" +
+            "  `timezone` varchar(100) DEFAULT NULL,\n" +
+            "  `balance` double DEFAULT NULL,\n" +
+            "  `uuid` varchar(100) DEFAULT NULL,\n" +
+            "  `timestamp` int(50) DEFAULT NULL,\n" +
             "  PRIMARY KEY (`id`)\n" +
             ") ENGINE=InnoDB DEFAULT CHARSET=utf8;";
-    void createTables(){;
-        //executeSQL(sqlCrateChatLogTable);
 
-        mysql.execute(sqlMessageTable);
+    void createTables(){;
+
+        mysql.execute(sqlChatTable);
+        mysql.execute(sqlLoginTable);
 
     }
+    //      ログメッセージ
+    void log(String text){
+        getLogger().info(text);
+    }
+    //     サーバーメッセージ
+    void serverMessage(String text){
+        //command("say "+text);
+        Bukkit.getServer().broadcastMessage(text);
+    }
 
+    /////////////////////////////////
+    //      ログインログ保存
+    /////////////////////////////////
+    void  insertLoginLog(String server,String world,String name,String ip,String country,String city,String timezone,double balance,String uuid){
+        long curTime = System.currentTimeMillis() / 1000L;
+        mysql.execute("insert into login_log values(0,'"+server+ "','"+world+ "','"+name
+                        +"','" + ip
+                        +"','" + country
+                        +"','" + city
+                        +"','" + timezone
+                        +"'," + balance
+                        +",'" + uuid
+                        +"'," + curTime
+                        +");");
 
-    void  insertMessage(String server,String world,String name,String message){
-        String time = "1234";
-        long currTime = System.currentTimeMillis() / 1000L;
+    }
+    //////////////////////////////////
+    //      チャットログ保存
+    //////////////////////////////////
+    void  insertChatLog(String server,String world,String name,String message){
 
-        mysql.execute("insert into chat_log values(0,'"+server+ "','"+world+ "','"+name+ "','" + message + "','" + currTime + "');");
+        long curTime = System.currentTimeMillis() / 1000L;
+        mysql.execute("insert into chat_log values(0,'"+server+ "','"+world+ "','"+name+ "','" + message + "'," + curTime + ");");
+    }
 
-        return;
-                /*
+    void showChatLog(){
+                        /*
         final ResultSet rs = mysql.query("SELECT * FROM `messages` LIMIT 500");
         try
         {
@@ -124,8 +194,8 @@ public final class Man10Core extends JavaPlugin implements Listener {
             this.getLogger().info("Error executing a query: " + e.getErrorCode());
         }
 */
-
     }
+
     //      基本表示系
 
 
